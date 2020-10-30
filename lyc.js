9980321aa9506e85400c9c49353f7883b90066a4
Schwarzkopf-Henkal <1198168788@qq.com>
2020-10-30 15:44:28 +0800

    lowspeed

--- lyc.js
+++ lyc.js
@@ -74,18 +74,23 @@ $(function() {
         { "name": "github", "function": github }
     ];
     var Nekos = [
-        { "url": "https://cdn.jsdelivr.net/gh/Schwarzkopf-Henkal/CDN/NK2.jpeg" },
-        { "url": "https://cdn.jsdelivr.net/gh/Schwarzkopf-Henkal/CDN/N1.png" },
-        { "url": "https://cdn.jsdelivr.net/gh/Schwarzkopf-Henkal/CDN/N2.png" },
-        { "url": "https://cdn.jsdelivr.net/gh/Schwarzkopf-Henkal/CDN/N3.png" }
+        { "url": "https://s1.ax1x.com/2020/10/30/BtYtvF.png" },
+        { "url": "https://s1.ax1x.com/2020/10/30/BtYUu4.png" },
+        { "url": "https://s1.ax1x.com/2020/10/30/BtYaDJ.png" },
+        { "url": "https://s1.ax1x.com/2020/10/30/BtYJ3T.jpg" },
+        { "url": "https://s1.ax1x.com/2020/10/30/BttBLQ.png" },
+        { "url": "https://s1.ax1x.com/2020/10/30/BttrZj.png" },
+        { "url": "https://s1.ax1x.com/2020/10/30/Btt0sg.png" },
+        { "url": "https://s1.ax1x.com/2020/10/30/Bttyon.png" },
+        { "url": "https://s1.ax1x.com/2020/10/30/Bttciq.png" },
+        { "url": "https://s1.ax1x.com/2020/10/30/BtN86U.png" }
     ]
     terminal.on('click', () => {
         input.focus();
     })
     input.on('keypress', function(key) {
             if (key.keyCode == 13) {
-                print(input.val(), true);
-                input.val('');
+                Run(input.val());
             } else if (input.val()[0] == '\n')
                 input.val('');
         })
@@ -124,15 +129,20 @@ $(function() {
         input_box.before(`<div class="output_box"><img src="${url}" style="width:500px;"></div>`);
     }
 
+    function Run(cmd) {
+        cmd = cmd.replace('\n', '');
+        print(cmd, true);
+        input.val('');
+        RunCmd(cmd.replace(/ +/, ''));
+    }
+
     function print(cmd, usr) {
+
         if (usr === true) {
             input_box.before('<div class="output_box"><pre class="output_span"><span class="user_info">Ubuntu@49.234.17.22</span><span class="user_info">:</span><span class="user_info">~</span><span class="user_info">$ </span><span class="output">' + cmd + '</span></pre></div>');
         } else {
             input_box.before('<div class="output_box"><pre class="output_span"><span class="output">' + cmd + '</span></pre></div>');
         }
-        input.val('');
-        if (usr === true)
-            RunCmd(cmd);
     }
 
     function RunCmd(cmd) {
